eb960e5f20d62ac0bd66630b1df0e0f11788302a
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-01-04 15:31:43 +0000

    datatypessummary

---
+++ 01_basics/06_datatypes-summary.js
@@ -0,0 +1,21 @@
+//Primitive(call by value)
+//7 types:string ,number ,boolean ,null ,undefined ,symbol ,BigInt
+
+const score=100
+const scorevalue=100.3//number
+
+const isloggedin=false//boolean
+
+const outsidetemp=null//null
+
+let userEmail;//undefined
+
+const id= Symbol('123')
+const id2=Symbol('123')//symbol
+console.log(id == id2);
+
+const bignumber=6432457865434578n//BigInt
+
+//Reference/Non primitive(call by reference)
+//array, objects, functions
+
