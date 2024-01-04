c8477ca4c50b0e21d3a91bba6d9d18df439be121
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-01-04 15:32:56 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,23 @@
+let str1 = "hello"
+let str2 = " harustat"
+
+let str3 = str1 + str2
+
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+//console.log((3+4)*5%3);
+
+//console.log(true);
+//console.log(+true);
+//console.log(+"");
+
+let n1,n2,n3
+n1=n2=n3=2+2
+
+let gameCounter=100
+gameCounter++; //or ++gameCounter;
+console.log(gameCounter);
