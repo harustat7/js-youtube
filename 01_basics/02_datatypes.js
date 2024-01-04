1cd78ff2e4757b7e1a1cd1bfdf3badeefcf88cd8
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-01-04 15:32:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all js code as newer version//
+
+//alert(3 + 3)// we are using node js,not browser
+
+console.log(3
+    +
+    3); //code readability should be high
+
+console.log("Harustat");
+
+let name="Manish"
+let age="20"
+let isLoggedIn=false
+let state;
+
+//primitive datatypes
+//number => 2^53
+//bigint=>for big numbers
+//string => ""
+//boolean=> true or false
+//null=>standalone value(empty value)
+//undefined=>didn't define state
+//symbol=>for uniqueness
+
+
+//object
+
+console.log(typeof "Manish");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
