fc4bcea9ae96c1694557daf42e390d183ea94225
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-01-20 12:08:40 +0000

    arrays

---
+++ 02_basics/02_arrays2.js
@@ -0,0 +1,41 @@
+
+//array in array(merging of 2 arrays in one array)
+const marvel_heros=["thor","ironman","spiderman"]
+const dc=["superman","flash","batman"]
+/*
+//using push
+marvel_heros.push(dc)
+console.log(marvel_heros);
+//to access specific position in this case
+console.log(marvel_heros[3][1]);
+
+
+//using concate-returns a new array
+const allheros=marvel_heros.concat(dc)
+console.log(allheros);
+
+
+//using spread
+const all_new_heros = [...marvel_heros,...dc]
+console.log(all_new_heros);
+
+
+const another_array =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const usable_array = another_array.flat(Infinity)
+console.log(usable_array);
+
+
+//for data scrapping
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("Hitesh"))
+console.log(Array.from({name:"hitesh"}));//empty array(interesting case for interviews)
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+//forms an array
+console.log(Array.of(score1,score2,score3));
+*/
