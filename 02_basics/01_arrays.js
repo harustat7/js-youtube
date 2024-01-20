0517062380ccec3b2b3029ca588fe19b4fa86791
harustat7 <125195067+harustat7@users.noreply.github.com>
2024-01-20 10:42:30 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,50 @@
+//array
+
+//ways to declare and define arrays
+const myArr = [ 0,1,2,3,4,5, true,"hitesh"]//all types of datatypes are allowed in a siongle array
+const myHeros=["shaktiman","nagraj"]
+const myArr2 = new Array(1,2,3,4)
+/*
+console.log(myArr[1]);
+
+
+//array methods
+myArr.push(6)
+myArr.push(7)
+console.log(myArr);//new values 6 and 7 added
+
+myArr.pop()
+console.log(myArr);//last value 7 removed
+
+myArr.unshift(9);//added in starting
+console.log(myArr);
+myArr.shift()//removed from the starting
+console.log(myArr);
+
+console.log(myArr.includes(9));//boolean type
+console.log(myArr.indexOf(9));//number
+
+
+//conversion to string
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr);
+console.log(typeof newArr);
+
+
+//slice,splice
+
+console.log("A ",myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+//after slicing of original array-no change
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1,3)
+//after splicing of original array-change is that elements included in splice are cut from the original array
+console.log("C ", myArr);
+console.log(myn2);
+
+*/
+
